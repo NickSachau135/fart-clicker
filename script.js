9622e9840410c0be9c4304aed68ce5ebf4e4e4c0
Kevin Volkel <kevinator900@gmail.com>
2021-01-29 23:13:34 -0700

    prettied up the farts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let farts = 0;
+let fartRate = 0;
 
 let milk = 0;
 let milkBefore = 0;
@@ -32,38 +33,54 @@ function farting() {
     playsound()
 }
 
+// fartRate = (seconds/fart) = (fart / seconds)
+
 function milkItem() {
     if(farts >= 10) {
         farts -= 10;
         document.getElementById('farts').textContent = farts;
         milk++;
-        setInterval(function(){farting()}, 2500);
+        fartRate += 2500;
+        setInterval(function(){farting()}, fartRate);
+        document.getElementById("fartRate").textContent = `${(fartRate / 10000)} farts per seconds`
     }
 }
 
 function beansItem() {
     if(farts >= 100) {
+        farts -= 100;
         beans++;
-        setInterval(function(){farting()}, 2500);
+        fartRate += 5000;
+        setInterval(function(){farting()}, fartRate);
+        document.getElementById("fartRate").textContent = `${fartRate / 1000} farts per seconds`
     }
 }
 
 function energyItem() {
     if(farts >= 500) {
+        farts -= 500;
         energy++;
-        setInterval(function(){farting()}, 2500);
+        fartRate += 10000;
+        setInterval(function(){farting()}, fartRate);
+        document.getElementById("fartRate").textContent = `${fartRate / 1000} farts per seconds`
     }
 }
 function coffeeItem() {
     if(farts >= 1000) {
+        farts -= 1000;
         energy++;
-        setInterval(function(){farting()}, 2500);
+        fartRate += 20000;
+        setInterval(function(){farting()}, fartRate);
+        document.getElementById("fartRate").textContent = `${fartRate / 1000} farts per seconds`
     }
 }
 function laxativeItem() {
     if(farts >= 5000) {
+        farts -= 5000;
         energy++;
-        setInterval(function(){farting()}, 2500);
+        fartRate += 50000;
+        setInterval(function(){farting()}, fartRate);
+        document.getElementById("fartRate").textContent = `${fartRate / 1000} farts per seconds`
     }
 }
 
